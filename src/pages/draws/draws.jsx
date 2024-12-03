b849c92dcc3f29fdc038e7faa1773dadbfaf4cd7
Brickhu <brickhu@126.com>
2024-12-04 01:57:00 +0800

    updating darws page

--- src/pages/draws/draws.jsx
+++ src/pages/draws/draws.jsx
@@ -7,16 +7,17 @@ import { Datetime } from "../../components/moment"
 import { InfoItem } from "../../components/infoitem"
 import { shortStr } from "../../lib/tool"
 import { ShareToSocial } from "../../components/share"
-import { currency } from "../../signals/global"
+import Loadmore from "../../components/loadmore"
+
 
 
 const DrawItem = props => {
   const [{item:{round,lucky_number,matched,jackpot,created,winners,taxation,id,ticker,denomination}},rest] = splitProps(props,["item"])
 
   return (
-    <section class="response_cols border-b border-current/20 first:border-t py-12 px-1">
+    <section class="response_cols border-b border-current/20 first:border-t py-10  px-4 hover:bg-current/2">
         <div class="col-span-full lg:col-span-2">
-          <span class="text-2xl">R{round}</span>
+          <span class="text-2xl">Round-{round}</span>
         </div>
         <div class="col-span-full lg:col-span-6 flex flex-col gap-2">
           <InfoItem label={"Winning Numbers"}>
@@ -29,17 +30,26 @@ const DrawItem = props => {
               {/* <span class="text-current/50">0 Matched bet</span> */}
             </div>
           </InfoItem>
-          <InfoItem label={"Result"}>No matched bets</InfoItem>
+          <InfoItem label={"Matched Bets"}><span classList={{
+            "text-current/50" : matched<=0,
+            "text-current" : matched>0
+          }}>{matched}</span></InfoItem>
           <InfoItem label={"Drawing Time"}><Datetime ts={Number(created)}/></InfoItem>
           <InfoItem label={"Taxation / Rate"}>{toBalanceValue(taxation,denomination||6,2)} <span class="text-current/50">${ticker}</span> / 0.1</InfoItem>
           <InfoItem label={"Drawing Id"}><span class="inline-flex items-center gap-2">{shortStr(id,6)} <Icon icon="ei:external-link"></Icon></span></InfoItem>
         </div>
         <div class="col-span-full lg:col-span-4 flex flex-col justify-between items-end">
-          <div class="flex flex-col gap-2 w-full">
-            <div class ="text-current/50 uppercase">The Prize</div>
-            <div><span >🏆 <span class="text-xl">{toBalanceValue(jackpot,denomination||6,2)}</span></span> <span class="text-current/50">${ticker}</span></div>
-            <div class="px-1 inline-flex gap-2"><Icon icon="ph:arrow-elbow-down-right-light"></Icon> <span>{winners}</span><span class="text-current/50">{winners>1?"winner":"winners"}</span> </div>
-            <div class="px-1 inline-flex gap-2"><Icon icon="ph:arrow-elbow-down-right-light"></Icon> <span>{winners}</span><span class="text-current/50">{winners>1?"winner":"winners"}</span> </div>
+          <div class="flex flex-col gap-4 w-full">
+            
+            <div>
+              <div class ="text-current/50 uppercase">The Prize</div>
+              <div class="flex items-center"><span>🏆 <span class="text-2xl">{toBalanceValue(jackpot,denomination||6,2)}</span></span> <span class="text-current/50">${ticker}</span></div>
+            </div>
+            <div class="px-1 inline-flex gap-2 items-center"><Icon icon="ph:arrow-elbow-down-right-light"></Icon> <div class="inline-flex py-1 px-3 bg-third text-third-content rounded-full gap-2"><span>{winners} </span><span class="text-current/50">{winners>1?"Winners shared":"Winner takes"}</span></div>  </div>
+            <div class="flex gap-2 px-1">
+              <Icon icon="proicons:info" />
+              <span class="text-xs text-current/50">No matching bets this round, the last bettor takes the entire prize.</span>
+            </div>
           </div>
           <ShareToSocial/>
         </div>
@@ -48,7 +58,7 @@ const DrawItem = props => {
 }
 
 export default props => {
-  const [draws,{hasMore}] = createDraws(()=>({pool_id:pool?.id}))
+  const [draws,{hasMore,loadingMore,loadMore}] = createDraws(()=>({pool_id:pool?.id}))
   createEffect(()=>console.log("draws",draws()))
   return (
     <section class="flex flex-col py-8 ">
@@ -57,6 +67,10 @@ export default props => {
           return <DrawItem item={item}/>
         }}
       </For>
+      <Show when={hasMore()}>
+          <Loadmore loadMore={loadMore} loading={loadingMore()}/>
+        </Show>
     </section>
+    
   )
 }
