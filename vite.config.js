8f983adabd9b727b77d513907d0453b2e45c1377
Brickhu <brickhu@126.com>
2024-12-28 12:31:25 +0800

    删除测试页面，更新依赖项和构建脚本，优化 Vite 配置

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,47 @@ export default defineConfig({
           if (id.includes('bignumber.js')) {
             return 'bignumber.js'
           }
+          if (id.includes('solid-js')) {
+            return 'solid-js'
+          }
+          if (id.includes('@solidjs/router')) {
+            return '@solidjs/router'
+          }
+          if (id.includes('solid-toast')) {
+            return 'solid-toast'
+          }
+  
+          if (id.includes('@iconify-icon/solid')) {
+            return '@iconify-icon/solid'
+          }
+          if (id.includes('solid-js/web')) {
+            return 'solid-js/web'
+          }
+          if (id.includes('balls.jsx')) {
+            return 'balls.jsx'
+          }
+          if (id.includes('bip39')) {
+            return 'bip39'
+          }
+          if (id.includes('node-forge')) {
+            return 'node-forge'
+          }
+          if (id.includes('iconify-icon')) {
+            return 'iconify-icon'
+          }
+          if (id.includes('axios')) {
+            return 'axios'
+          }
+          if (id.includes('buffer/index.js')) {
+            return 'buffer/index.js'
+          }
+          if (id.includes('arweave-wallet-connector/lib/index.js')) {
+            return 'arweave-wallet-connector/lib/index.js'
+          }
+          if (id.includes('warp-arbundles/build/web/esm/bundle.js')) {
+            return 'warp-arbundles/build/web/esm/bundle.js'
+          }
+   
           if (id.includes('node_modules')) {
             return 'vendor'
           }
